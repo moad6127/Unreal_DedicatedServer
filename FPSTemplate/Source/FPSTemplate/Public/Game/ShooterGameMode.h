8f9206937c0c9c79000b0a2281bfe1b2539039d5
moad6127 <moad6127@gmail.com>
2025-03-24 20:44:04 +0900

    Feat : DedicatedServer
    GameLiftPlugin 추가하기 + GameMode클래스 생성하기

---
+++ FPSTemplate/Source/FPSTemplate/Public/Game/ShooterGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/ShooterGameModeBase.h"
+#include "ShooterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTEMPLATE_API AShooterGameMode : public AShooterGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
